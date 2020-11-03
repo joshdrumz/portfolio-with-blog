35dfd251729314940bc00bb269554e63a4a46cfa
Josh Arrants <duhhobo@gmail.com>
2020-11-03 12:07:26 -0500

    Update blog page view

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -8,81 +8,76 @@
       <v-divider></v-divider>
     </v-responsive>
 
-    <!-- <v-row v-for="edge in $page.blogs.edges" :key="edge.node.id">
-      
-        <v-hover v-slot:default="{ hover }">
-          <v-card
-            class="my-1 v-card-zoom"
-            rounded="xl"
-            dark
-            :elevation="hover ? 16 : 4"
-            :ripple="{ class: 'green--text' }"
-            @click="$router.push(`${edge.node.path}`)"
+    <div v-for="edge in $page.blogs.edges" :key="edge.node.id">
+      <v-row>
+        <v-col md="4">
+          <v-img
+            :src="edge.node.image"
+            :alt="edge.node.image_caption"
+            max-width="300"
           >
-            <v-list-item>
-              <v-list-item-avatar
-                v-if="edge.node.category.title === 'Coding'"
-                color="green"
-              >
-                <v-icon>mdi-xml</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-avatar
-                v-else-if="edge.node.category.title === 'Personal'"
-                color="green"
-              >
-                <v-icon>mdi-face</v-icon>
-              </v-list-item-avatar>
-              <v-list-item-content>
-                <v-list-item-title class="headline">{{
-                  edge.node.title
-                }}</v-list-item-title>
-                <v-list-item-subtitle
-                  >{{ edge.node.author.name }} |
-                  {{ formatDate(edge.node.created) }}</v-list-item-subtitle
+            <template v-slot:placeholder>
+              <v-row class="fill-height ma-0" align="center" justify="center">
+                <v-progress-circular
+                  indeterminate
+                  color="grey lighten-5"
+                ></v-progress-circular>
+              </v-row>
+            </template>
+          </v-img>
+        </v-col>
+        <v-col md="8">
+          <v-card rounded="xl" elevation="0">
+            <v-hover v-slot:default="{ hover }">
+              <v-card-title>
+                <g-link
+                  :to="edge.node.path"
+                  class="black--text"
+                  :class="hover ? 'text-decoration' : 'text-decoration-none'"
                 >
-              </v-list-item-content>
-            </v-list-item>
+                  {{ edge.node.title }}
+                </g-link>
+              </v-card-title>
+            </v-hover>
 
-            <v-img
-              :src="edge.node.image"
-              :alt="edge.node.image_caption"
-              height="300"
-            >
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular
-                    indeterminate
-                    color="grey lighten-5"
-                  ></v-progress-circular>
-                </v-row>
-              </template>
-            </v-img>
+            <v-card-subtitle>
+              {{ formatDate(edge.node.created) }} |
+              <g-link
+                :to="edge.node.category.path"
+                class="text-decoration-none green--text text--darken-4"
+              >
+                {{ edge.node.category.title }}
+              </g-link>
+            </v-card-subtitle>
 
             <v-card-text>
               {{ edge.node.excerpt }}
             </v-card-text>
-          </v-card>
-        </v-hover>
-      </v-col>
-    </v-row> -->
 
-    <v-row v-for="edge in $page.blogs.edges" :key="edge.node.id">
+            <!-- <v-divider class="ml-4"></v-divider> -->
+          </v-card>
+        </v-col>
+      </v-row>
+    </div>
+
+    <!-- <v-row v-for="edge in $page.blogs.edges" :key="edge.node.id">
       <v-col>
+        <v-img
+          :src="edge.node.image"
+          :alt="edge.node.image_caption"
+          class="d-flex"
+          height="300"
+        >
+          <template v-slot:placeholder>
+            <v-row class="fill-height ma-0" align="center" justify="center">
+              <v-progress-circular
+                indeterminate
+                color="grey lighten-5"
+              ></v-progress-circular>
+            </v-row>
+          </template>
+        </v-img>
         <v-card rounded="xl" elevation="0">
-          <!-- <v-img
-              :src="edge.node.image"
-              :alt="edge.node.image_caption"
-              height="300"
-            >
-              <template v-slot:placeholder>
-                <v-row class="fill-height ma-0" align="center" justify="center">
-                  <v-progress-circular
-                    indeterminate
-                    color="grey lighten-5"
-                  ></v-progress-circular>
-                </v-row>
-              </template>
-            </v-img> -->
           <v-hover v-slot:default="{ hover }">
             <v-card-title>
               <g-link
@@ -112,7 +107,7 @@
           <v-divider class="ml-4"></v-divider>
         </v-card>
       </v-col>
-    </v-row>
+    </v-row> -->
   </Layout>
 </template>
 
