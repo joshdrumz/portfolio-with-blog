7d9beded266b18fd8c305aec2b86eb214ec90e0a
Josh Arrants <duhhobo@gmail.com>
2020-11-06 13:24:05 -0500

    Add contact form using Netlify forms

---
+++ src/pages/Contact.vue
@@ -0,0 +1,70 @@
+<template>
+  <Layout>
+    <div>
+      <form
+        name="contact"
+        method="POST"
+        @submit.prevent="handleSubmit"
+        action="/success/"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+      >
+        <input type="hidden" name="form-name" value="contact" />
+        <p hidden>
+          <label> Don't fill this out: <input name="bot-field" /> </label>
+        </p>
+        <div class="sender-info">
+          <div>
+            <label for="name" class="label">Your name</label>
+            <input type="text" name="name" v-model="formData.name" />
+          </div>
+          <div>
+            <label for="email">Your email</label>
+            <input type="email" name="email" v-model="formData.email" />
+          </div>
+        </div>
+
+        <div class="message-wrapper">
+          <label for="message">Message</label>
+          <textarea name="message" v-model="formData.message"></textarea>
+        </div>
+
+        <button type="submit">Submit form</button>
+      </form>
+    </div>
+  </Layout>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formData: {}
+    };
+  },
+  methods: {
+    encode(data) {
+      return Object.keys(data)
+        .map(
+          key => encodeURIComponent(key) + '=' + encodeURIComponent(data[key])
+        )
+        .join('&');
+    },
+    handleSubmit(e) {
+      fetch('/contact', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+        body: this.encode({
+          'form-name': e.target.getAttribute('name'),
+          ...this.formData
+        })
+      })
+        .then(() => this.$router.push('/success'))
+        .catch(err => console.log(err));
+    }
+  }
+};
+</script>
+
+<style scoped>
+</style>
